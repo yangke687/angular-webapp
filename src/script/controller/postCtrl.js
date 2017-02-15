7bd75d0cfcd035858876b30ec383db243aeeafee
Yang Ke <saint_yk@163.com>
2017-02-15 21:37:17 +0800

    static favorite(我的收藏)  && post(投递记录) page

--- src/script/controller/postCtrl.js
+++ src/script/controller/postCtrl.js
@@ -2,5 +2,14 @@
 
 angular.module('app')
 	.controller('postCtrl', ['$scope', function($scope) {
-
+		$scope.tabList = [{
+			id: 'all',
+			name: '全部',
+		}, {
+			id: 'pass',
+			name: '邀请面试',
+		}, {
+			id: 'fail',
+			name: '不合适'
+		}];
 	}]);
