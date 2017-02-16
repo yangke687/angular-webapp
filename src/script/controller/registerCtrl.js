3aee7af52f479dc7deae8e3c66495ef9223ba847
YangKe <saint_yk@163.com>
2017-02-16 15:34:39 +0800

    register logic

--- src/script/controller/registerCtrl.js
+++ src/script/controller/registerCtrl.js
@@ -1,8 +1,27 @@
 'use strict';
 
 angular.module('app')
-	.controller('registerCtrl', ['$scope', function($scope) {
+	.controller('registerCtrl', ['$scope','$interval','$http', function($scope,$interval,$http) {
 		$scope.submit = function() {
 			console.log($scope.user);
-		}
+		};
+		$scope.send = function(){
+			$http.get('/data/code.json').then(function(res){
+				if(res.data.state===1){
+					var count = 60;
+					$scope.time = '60秒';
+					var interval = $interval(function(){
+						if(count <=0){
+							$interval.cancel(interval);
+							$scope.time = '';
+							return;
+						}
+						count--;
+						$scope.time = count + '秒';
+					},1000);
+				}
+			},function(err){
+				// error handling...
+			});
+		};
 	}]);
