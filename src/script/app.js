e44a3d29b0cfc5d8ca15152ba8f134fec68e68f6
Yang Ke <saint_yk@163.com>
2017-02-20 21:33:30 +0800

    add .enter .enter-active css animateion style

--- src/script/app.js
+++ src/script/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['ui.router', 'ngCookies', 'validation']).run(['$rootScope', function($rootScope) {
+angular.module('app', ['ui.router', 'ngCookies', 'validation', 'ngAnimate']).run(['$rootScope', function($rootScope) {
 	$rootScope.im = function() {
 		console.log('im');
 	}
